798d2d63a7f1130adf5c89c3a297358c2576c77f
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 11:43:06 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дан файл numsTask2.txt со словами расположенными в столбик.
+// Из заданных слов составьте одну длинную строку(разделить каждое слово пробелом);
+
+using System;
+using System.IO;
+
+class work_6_2
+{
+    static void Main()
+    {
+        string[] lines = File.ReadAllLines("numsTask2.txt"); 
+        string wordsLine = string.Join(" ", lines); 
+
+        Console.WriteLine(wordsLine); 
+    }
+}
+
